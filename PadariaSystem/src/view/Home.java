ce0a02f7bf672859f519be6d2b22c4c9ff22d4fb
MATHEUS RODRIGUES VIEIRA <matheus.rvieira5@senacsp.edu.br>
2025-03-18 21:21:12 -0300

    Itens para ambiente

---
+++ PadariaSystem/src/view/Home.java
@@ -0,0 +1,17 @@
+package view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import java.io.IOException;
+import static javafx.application.Application.launch;
+
+public class Home extends Application {
+    public void start (Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+}
