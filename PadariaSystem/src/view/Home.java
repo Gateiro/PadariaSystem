1f77e8982f44cdeef391863839fb3e2a4f24e981
MATHEUS RODRIGUES VIEIRA <matheus.rvieira5@senacsp.edu.br>
2025-03-25 21:55:22 -0300

    Atualização Home

--- PadariaSystem/src/view/Home.java
+++ PadariaSystem/src/view/Home.java
@@ -1,17 +1,31 @@
 package view;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import java.io.IOException;
-import static javafx.application.Application.launch;
+
 
 public class Home extends Application {
     public void start (Stage stage) throws Exception {
+        //Carregamento de imagens
+        Image imgIconStarbucks = new Image(getClass().getResourceAsStream("/view/img/starbucks96px.png"));
+        Image imgBackground = new Image(getClass().getResourceAsStream("/view/img/"+"background1.jpg"));
+
+        BorderPane layoutPrincipal = new BorderPane();
+        layoutPrincipal.setStyle("-fx-background-image: url('/view/img/"+"background1.jpg');");
+
+        Scene scene = new Scene(layoutPrincipal, 1024, 700);
+        stage.setScene(scene);
+        stage.setTitle("Starbucks");
+        stage.getIcons().add(imgIconStarbucks);
+        stage.show();
 
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    public static void main(String[] lifecyle) {
+        launch(lifecyle); //lifecycle --> init(), start(), stop()
 
     }
 }
